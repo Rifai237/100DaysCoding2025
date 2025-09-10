7b994a184fb363fbf31cda9fbc484c9070848083
Rifai237 <pairifai050@gmail.com>
2025-09-10 18:00:26 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,9 @@
+public class day3 {
+    public static void main(String[] args) {
+        //komentar satu baris
+        /* ini adalah sebuah komemtar yang di gunakan untuk mendeskripsikan sesuatu dengan kalimat yang panjang atau kalimat dengan
+        menggunakan beberapa baris
+         */
+
+    }
+}

26bcd01914280fa5b6fa085073f688e809abff69
Rifai237 <pairifai050@gmail.com>
2025-09-11 17:33:52 +0800

    Create days4.java

---
+++ days4.java
@@ -0,0 +1,12 @@
+public class day4 {
+    public static void main(String... args) {
+        System.out.println("emote love \\u2764"); //emot love
+        System.out.println(" \n haloo...");// baris baru
+        System.out.println("Nama : \t Muhrifai "); // sama seperti tab
+        System.out.println("tanggal lahir = 27\\Agustus\\2025");//digunakan menampilkan backslash
+        System.out.println("he say \"anjay\" ");//digunakan untuk tanda kutip
+        System.out.println("hal\booo");//untuk mengembalikan kata setelahnya
+     
+
+    }
+}

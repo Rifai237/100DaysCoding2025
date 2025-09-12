b41a73f50752d64e169d35443fbdcef1791a683b
Rifai237 <pairifai050@gmail.com>
2025-09-12 18:28:22 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class day5 {
+    public static void main(String[] args) {
+        byte umur = 19;
+        short umurkakek = 178;
+        int umurkakeknyakakek= 20000;
+        long umurnyakakeknyakakeknyakakek = 1217281638;
+
+        System.out.println("umurku :\t" + umur);
+        System.out.println("umurkakekku :\t" + umurkakek);
+        System.out.println("umurnyakakeknyakakekku :\t" + umurkakeknyakakek);
+        System.out.println("umurnyakakeknyakakeknyakakekku :\t" + umurnyakakeknyakakeknyakakek);
+    }
+}

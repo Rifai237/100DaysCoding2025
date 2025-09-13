0dbebaf8f1971aa0ea5be910e07117c808da4c49
Rifai237 <pairifai050@gmail.com>
2025-09-13 19:26:11 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,9 @@
+public class day6 {
+    public static void main(String[] args) {
+        float harga_baju = 100000.00f;
+        double harga_celana = 120000.00;
+
+        System.out.println("harga sebuah baju :\t" +harga_baju);
+        System.out.println("harga sebuah celana :\t" +harga_celana);
+    }
+}

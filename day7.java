f59820761cd4ea99ca497a822d87ba799a786ef9
Rifai237 <pairifai050@gmail.com>
2025-09-14 14:21:56 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,12 @@
+public class day7 {
+    public static void main(String[] args) {
+        char awalanNIM = 'D';
+        boolean pernyataan = true; // nilai true cuman ada dua yaitu 'true' dan 'false'
+
+        System.out.println("awalan nim mahasiswa teknik =\t" + awalanNIM);
+        System.out.println("apakah pernyataan ini betul =\t" + pernyataan);
+
+
+
+    }
+}

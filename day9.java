05cd1373fd9f71c419e9bfc0c33af4531c2968ed
Rifai237 <pairifai050@gmail.com>
2025-09-16 22:11:49 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,23 @@
+public class day9 {
+    public static void main(String[] args) {
+        //deklarasi
+        double tinggi_badan;
+        byte umur;
+
+        //menginisialisasi
+        tinggi_badan = 165.9;
+        umur = 17;
+        System.out.println("ini tinggi badan saya sebelum kuliah : " + tinggi_badan);
+        System.out.println("ini umur saya sebelum kuliah : " + umur);
+
+        //update variabel
+        tinggi_badan = 170.0;
+        umur =19;
+
+        System.out.println("ini tinggi badan saya sesudah kuliah : " + tinggi_badan);
+        System.out.println("ini umur saya sesudah kuliah : " + umur);
+
+
+
+    }
+}

43606c4b4d356ec1d801f86f27cc3faf75786bf8
Rifai237 <pairifai050@gmail.com>
2025-09-17 20:10:40 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,7 @@
+public class day10 {
+    public static void main(String[] args) {
+        final int tahun_lahir = 2006; // variabel inni tidak dapat di update lagi dikarenakan ini adalah variabel final
+
+        System.out.println("saya lahir tahun : " + tahun_lahir);
+    }
+}

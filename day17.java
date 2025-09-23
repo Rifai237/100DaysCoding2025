86593bac93371edae34150ad5efb5e0bfedaefdf
Rifai237 <pairifai050@gmail.com>
2025-09-24 00:06:40 +0800

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,14 @@
+
+
+public class day17 {
+    public static void main(String[] args) {
+       int angka1 = 9;
+       int angka2 = 100;
+       
+       int modulus = angka1 % angka2;
+
+        System.out.println("sisa baginya adalah :" + modulus);
+       
+        
+    }
+}

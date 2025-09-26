917f4d8b8e6e8a10c9aaf0ea9fd6da309eb21852
Rifai237 <pairifai050@gmail.com>
2025-09-26 20:47:15 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,11 @@
+public class day19 {
+    public static void main(String[] args) {
+        double angkaDouble = 128.99;
+        int angkaInt = (int) angkaDouble;
+        byte angkabyte = (byte) angkaInt;
+
+        System.out.println("Nilai double: " + angkaDouble);
+        System.out.println("Nilai int: " + angkaInt);
+        System.out.println("Nilai short: " + angkabyte);
+    }
+}

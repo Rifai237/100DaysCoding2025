c4a7faffe2abc890cf876c1539a6de1fb5d560a0
Rifai237 <pairifai050@gmail.com>
2025-09-27 23:33:35 +0800

    Create day20.java

---
+++ day20.java
@@ -0,0 +1,9 @@
+public class day20 {
+    public static void main(String[] args) {
+        int angka = 100;
+        String teks = String.valueOf(angka);
+
+        System.out.println("Angka sebagai String: " + teks);
+
+    }
+}

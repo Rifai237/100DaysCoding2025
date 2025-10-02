ff6273b9997dba8d82b722eea8f6b7fc214e80e1
Rifai237 <pairifai050@gmail.com>
2025-10-02 19:35:05 +0800

    Create day25.java

---
+++ day25.java
@@ -0,0 +1,14 @@
+public class day25 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Nilai awal a : " + a);
+        System.out.println("Nilai awal b : " + b);
+
+        System.out.println(" -- a = " + (--a));
+        System.out.println("setelah -- = " + a);
+        System.out.println(" ++ a = " + (b++));
+        System.out.println("setelah ++ = " + b);
+    }
+}

394709fa6cfa3eb7c176913ce8e9c935a7020281
Rifai237 <pairifai050@gmail.com>
2025-10-03 23:17:27 +0800

    Create day26.java

---
+++ day26.java
@@ -0,0 +1,17 @@
+public class day26 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        a +=2;
+        System.out.println(" += :" + a);
+        a -=2;
+        System.out.println(" -= :" + a);
+        a *=2;
+        System.out.println(" *= :" + a);
+        a /=2;
+        System.out.println(" /= :" + a);
+        a %=2;
+        System.out.println(" %= :" + a);
+
+    }
+}

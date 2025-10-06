7418eb5c37f96045193df8eabd1f9576da8faa69
Rifai237 <pairifai050@gmail.com>
2025-10-06 21:25:25 +0800

    Create day29.java

---
+++ day29.java
@@ -0,0 +1,10 @@
+public class day29 {
+    public static void main(String[] args) {
+        byte a = 9;
+        byte b= 10;
+
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        
+    }
+}

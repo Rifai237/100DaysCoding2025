cbe887ea923b2e531b90fdf6c96202b3acc70a6a
Rifai237 <pairifai050@gmail.com>
2025-10-07 22:48:57 +0800

    Create day30.java

---
+++ day30.java
@@ -0,0 +1,15 @@
+public class day30 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 5;
+        int d = 7;
+
+        System.out.println(a <= c);
+        System.out.println(a <= b);
+        System.out.println(b >= d);
+        System.out.println(b >= c);
+
+
+    }
+}

2f4d0dfc86aa68a539373663f2253dfc46b7d242
Rifai237 <pairifai050@gmail.com>
2025-10-12 23:00:56 +0800

    Create day35.java

---
+++ day35.java
@@ -0,0 +1,11 @@
+public class day35 {
+    public static void main(String... args) {
+        boolean benar = true;
+        if (benar != false ) {
+            System.out.println("Benar ");
+        } else   {
+            System.out.println("salah");
+        }
+
+    }
+}

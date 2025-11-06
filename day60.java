cfba4b2c96631db0fa4f17d9996666a686e4427d
Rifaiii <pairifai050@gmail.com>
2025-11-06 22:26:49 +0800

    Create day60.java

---
+++ day60.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class day60 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+        
+        System.out.println("\nAngka Ganjil  " + N + "-1 :");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        
+        System.out.println("\n\nAngka Genap " + N + "- 1:");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

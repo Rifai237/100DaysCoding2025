278d04e1196d3b3a485133b28bda4486b30be740
Rifaiii <pairifai050@gmail.com>
2025-11-08 15:50:57 +0800

    Create Day62.java

---
+++ Day62.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class day62 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int jumlah = 0;
+
+        System.out.print("Masukkan nilai N: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+             jumlah += i;
+        }
+
+        System.out.println("Jumlah dari 1 sampai " + n + " adalah " + jumlah);
+    }
+}

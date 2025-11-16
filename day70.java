dd1547dc4d1cb4faee73acf9d6239fe47f9926f4
Rifaiii <pairifai050@gmail.com>
2025-11-16 12:22:02 +0800

    Create day70.java

---
+++ day70.java
@@ -0,0 +1,13 @@
+public class day70 {
+    public static void main(String[] args) {
+        int tinggi = 4;
+        int lebar = 6;
+
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= lebar; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

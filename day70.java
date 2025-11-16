30ba2dc2bc3ed97bb2725c3c285de95e630a9f85
Rifaiii <pairifai050@gmail.com>
2025-11-16 12:15:56 +0800

    Update day70.java

--- day70.java
+++ day70.java
@@ -1,8 +1,33 @@
-public class Main {
-    
-    public static void main(String[] args){
-        
-        System.out.println("" );
+package evaluasi;
+
+import java.util.Scanner;
+
+public class empat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah pola :");
+        int pola = sc.nextInt();
+
+        for (int i = 1; i <= pola; i++) {
+            for (int j = 1; j <= pola; j++) {
+                System.out.print("-+");
+
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= pola; i++) {
+            for (int j = 1; j <= pola; j++) {
+                System.out.print(" " +i);
+
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= pola; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" " + j);
+
+            }
+            System.out.println();
+        }
     }
-    
 }

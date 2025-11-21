5b2ca2bf9c9df670b0762a3e49a010e3786c31f8
Rifaiii <pairifai050@gmail.com>
2025-11-21 23:12:10 +0800

    Create day75.java

---
+++ day75.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Day75 {
+  public static void main(String[] args) {
+    
+    String nama = "Halo Dunia";
+    System.out.println(nama.length());
+    System.out.println(nama.toUpperCase());
+    System.out.println(nama.toLowerCase());
+  }
+}

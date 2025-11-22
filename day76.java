f526b6b954d02291c58414064fc08ea7a505b959
Rifaiii <pairifai050@gmail.com>
2025-11-22 23:09:06 +0800

    Create day76.java

---
+++ day76.java
@@ -0,0 +1,10 @@
+public class Day76 {
+  public static void main(String[] args) {
+    
+    String nama = "Muh Rifai";
+    System.out.println(nama.equals("muh rifai"));
+    System.out.println(nama.equalsIgnoreCase("muh rifai"));
+    System.out.println(nama.contains("rifai"));
+    System.out.println(nama.isEmpty());
+  }
+}

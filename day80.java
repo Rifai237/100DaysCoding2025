5b1a9520319c971198a9980240d3cef5090c3f72
Rifaiii <pairifai050@gmail.com>
2025-11-26 12:43:33 +0800

    Create day80.java

---
+++ day80.java
@@ -0,0 +1,8 @@
+public class day80 {
+    public static void main(String[] args) {
+        int array [] = {2,3,4,5,6};
+        for (int i = array.length-1; i >= 0 ; i--) {
+            System.out.print(array[i]);
+        }
+    }
+}

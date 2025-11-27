dc872bdfedd03ea7dc086a0cc7ea743316704c35
Rifaiii <pairifai050@gmail.com>
2025-11-27 21:36:47 +0800

    Create day81.java

---
+++ day81.java
@@ -0,0 +1,12 @@
+public class Day81 {
+  /*
+   * 100 Days Of Coding 2025
+   * Day 81 : Latihan: Akses Elemen Array Dengan Indeks
+   */
+  public static void main(String[] args) {
+    int nilaiUjian[] = { 70, 80, 90, 85, 75, 65, 50, 15, 20 };
+    System.out.println(nilaiUjian[2]); // 90
+    System.out.println(nilaiUjian[5]); // 65
+    System.out.println(nilaiUjian[7]); // 15
+  }
+}

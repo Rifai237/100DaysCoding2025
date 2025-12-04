90b823a8c1f1fe1d7d69a16c27265b9b429dc806
Rifaiii <pairifai050@gmail.com>
2025-12-04 22:35:04 +0800

    Create day88.java

---
+++ day88.java
@@ -0,0 +1,10 @@
+public class day88 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}

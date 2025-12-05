61c450ea7a103bec4c37307243375564b442b3cd
Rifaiii <pairifai050@gmail.com>
2025-12-05 21:41:47 +0800

    Create day89.java

---
+++ day89.java
@@ -0,0 +1,9 @@
+public class day89 {
+    static void namaku(String nama){
+        System.out.println("Haii Nama Saya :" + nama );
+    }
+
+    public static void main(String[] args) {
+        namaku("Rifai");
+    }
+}

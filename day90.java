5ee3f8dba601a078fdd6bbd7d0ed099363bebda9
Rifaiii <pairifai050@gmail.com>
2025-12-06 22:01:46 +0800

    Create day90.java

---
+++ day90.java
@@ -0,0 +1,12 @@
+
+
+public class day90 {
+    static public int kali(int a, int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        day90 aku = new day90();
+
+        System.out.println(kali(27,78));
+    }
+}

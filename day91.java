f94b74e1721c15e1efc605cb224ff769a3a4b795
Rifaiii <pairifai050@gmail.com>
2025-12-07 22:20:42 +0800

    Create day91.java

---
+++ day91.java
@@ -0,0 +1,14 @@
+public class Day91 {
+  /*
+   * 100 Days Of Coding 2025
+   * Day 91 : Parameter method
+   */
+
+  int bagi(int a, int b){
+    return a / b;
+  }
+  public static void main(String[] args) {
+    Day91 k = new Day91();
+    System.out.println(k.bagi(100, 5));
+  }
+}

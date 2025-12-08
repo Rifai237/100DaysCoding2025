89dcbd572cd3386122560eca189290188edc0654
Rifaiii <pairifai050@gmail.com>
2025-12-08 22:11:35 +0800

    Create day92.java

---
+++ day92.java
@@ -0,0 +1,15 @@
+public class Day92 {
+  /*
+   * 100 Days Of Coding 2025
+   * Day 92 : Keyword return
+   */
+
+  int kurang(int a, int b){
+    return a - b;
+  }
+  public static void main(String[] args) {
+    Day92 k = new Day92();
+    System.out.println(k.kurang(10, 5));
+  }
+
+}

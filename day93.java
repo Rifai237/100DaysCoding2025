6619ce12a3c7ee62dc19b6786ff43c693f753fda
Rifaiii <pairifai050@gmail.com>
2025-12-09 20:38:06 +0800

    Create day93.java

---
+++ day93.java
@@ -0,0 +1,10 @@
+public class day93 {
+    public static void main(String[] args) {
+        double x = 4.7;
+
+        System.out.println(Math.ceil(x));
+        System.out.println(Math.floor(x));
+        System.out.println(Math.round(x)); 
+    }
+}
+

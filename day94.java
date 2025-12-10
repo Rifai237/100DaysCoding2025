a78622ad400a596ac3d9a78ac2d64e3ee5f7a56c
Rifaiii <pairifai050@gmail.com>
2025-12-10 12:42:08 +0800

    Create day94.java

---
+++ day94.java
@@ -0,0 +1,11 @@
+public class day94 {
+    static void love (String aku){
+        System.out.println("halooooo " + aku);
+       
+    }
+
+    public static void main(String[] args) {
+        
+        love("akuuu");
+    }
+}

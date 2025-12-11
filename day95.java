9bb9c7de5e355ac76b6b6369a0c65a4001ef9cfa
Rifaiii <pairifai050@gmail.com>
2025-12-11 23:07:18 +0800

    Create day95.java

---
+++ day95.java
@@ -0,0 +1,9 @@
+public class day95 {
+   
+    static void cetak(String pesan){
+        System.out.println("Pesan: " + pesan);
+    }
+    public static void main(String[] args) {
+        cetak("selamat pagi");
+    }
+}

b319b2fe2735daee2cc6c02789fb31cddf463575
Rifaiii <pairifai050@gmail.com>
2025-12-16 23:11:57 +0800

    Create day100.java

---
+++ day100.java
@@ -0,0 +1,12 @@
+public class day100 {
+    public static void main(String[] args) {
+        int array [] = {1,2,3,4};
+        int total = 0;
+        for (int i = 0; i < array.length ; i++) {
+            total += array[i];
+
+           
+        }
+         System.out.println(total);
+    }
+}
